36cb5721362e26bd11016d82d3f349793134f679
vitalij5 <106188201+vitalij5@users.noreply.github.com>
2022-05-27 21:49:28 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = "5324380570:AAF26T2uPvKNw9cfXJAZgO2sGGV2wwOnzz8"
+
+CHANNEL_URL = "https://t.me/tutorialcw"
+CHANNEL_ID = "@Tutorialcw"
+CHAT_ID = "@tutorialcwchat"
+ADMIN_ID = "511693233"
+
+WORDS = ['лох', 'ютубер','вонючка','лошок','гном']
---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+import config as cfg
+
+# import markups as nav
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=cfg.TOKEN)
+dp = Dispatcher(bot)
+
+
+def check_sub_channel(chat_member):
+    return chat_member['status'] != "left"
+
+
+@dp.message_handler(content_types=["new_chat_members"])
+async def user_joined(message: types.Message):
+    await message.answer("Привет!\nМы-команда Супергероев 'Дом Ангела'.\nТеперь в нашем комьюнити на одного "
+                         "Супергероя стало больше."
+                         "\nПриятно познакомиться:)")
+
+
+# dp.message_handler()
+# async def mess_handler(message: types.Message):
+#    if check_sub_channel(await bot.get_chat_member(chat_id=cfg.CHANNEL_ID, user_id=message.from_user.id)):
+#        text = message.text.lower()
+#        for word in cfg.WORDS:
+#            if word in text:
+#                await message.delete()
+#    else:
+#        await message.answer("Чтобы отправлять сообщения подпишитесь на канал", reply_markup=nav.channelMenu)
+#       await message.delete()
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
